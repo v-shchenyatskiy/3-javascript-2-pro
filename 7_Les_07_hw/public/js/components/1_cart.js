d1f7c7b215e7000a80f27458b1b821833b7ea5ce
v-shchenyatskiy <60130407+v-shchenyatskiy@users.noreply.github.com>
2021-02-17 19:09:20 +0700

    7 lesson 07 hw (#7)
    ДЗ-7:
      - сделал сервер в одном файле (без роутера)
      - все действия логгируются
      - корзина подгружается

---
+++ 7_Les_07_hw/public/js/components/1_cart.js
@@ -0,0 +1,154 @@
+'use strict';
+
+const cartTop = {
+    name: 'cart-top',
+    template:  `<div class="cart__top">
+                    <span style="min-width: 10%;">id</span>
+                    <span>img</span>
+                    <span>Product</span>
+                    <span>Price</span>
+                    <span>Quan</span>
+                    <span>Amount</span>
+                    <span style="min-width: 5%;"></span>
+                </div>`
+};
+
+const cartBottom = {
+    name: 'cart-bottom',
+    props: ['total'],
+    template:  `<div class="cart__bottom">
+                    <span>Total:</span>
+                    <span id="cart_total">{{ total }} &#8381;</span>
+                </div>`
+};
+
+Vue.component('cart', {
+    components: {cartTop, cartBottom},
+    data() {
+        return {
+            cart: [],
+            showCart: false,
+        };
+    },
+    methods: {
+
+        addToCart(product, quantity) {
+            if (this.isProductInCart(product.id)) { return; }
+
+            product.quantity = quantity;
+            product.amount = product.price * product.quantity;
+        
+            product = Object.assign({}, product);
+
+            this.$root.httpJson('/api/cart', product, 'POST')
+                .then(response => {
+                    if (response.result === 1) {
+                        this.cart.push(product);
+                    }
+                });
+        },
+
+        deleteFromCart(product) {
+            this.$root.httpJson('/api/cart', product, 'DELETE')
+                .then(response => {
+                    if (response.result === 1) {
+                        this.cart.splice( this.cart.indexOf(product), 1 );
+                    }
+                });
+        },
+
+        increaseQuantity(id) {
+            this.$root.httpJson(`/api/cart/${id}`, {quantity: 1}, 'PUT')
+                .then(response => {
+                    if (response.result === 1) {
+                        let item = this.cart.find(item => item.id === id);
+                        item.quantity++;
+                        item.amount = item.price * item.quantity;
+                    }
+                });
+        },
+
+        decreaseQuantity(id) {
+            this.$root.httpJson(`/api/cart/${id}`, {quantity: -1}, 'PUT')
+                .then(response => {
+                    if (response.result === 1) {
+                        let item = this.cart.find(item => item.id === id);
+                        if (item.quantity > 1) {
+                            item.quantity--;
+                            item.amount = item.price * item.quantity;
+                        }
+                    }
+                });
+        },
+
+        isProductInCart(id) {
+            let item = this.cart.find(item => item.id === id);
+            if ( item ) { return true; } else { return false; }
+        },
+
+    },
+    computed: {
+        cartTotal() {
+            return this.cart.reduce( (sum, { amount }) => sum + amount, 0 );
+        },
+    },
+    template:  `<div class="cart_box">
+                    <button @click="showCart=!showCart" class="cart_btn" type="button">Корзина</button>
+                    <div class="cart" :class="{cart_hidden: !showCart}">
+                        <cart-top></cart-top>
+                        <cart-items :cart="cart"></cart-items>
+                        <cart-bottom :total="cartTotal"></cart-bottom>
+                    </div>
+                </div>`,
+    created() {
+        // this.$root.getJson('/api/cart-clear');
+    },
+    mounted(){
+        this.$root.getJson('/api/cart')
+            .then(cart => {
+                for( let item of cart.contents ) {
+                    this.cart.push(item);
+                }
+            });
+    },
+});
+
+
+const cartItem = {
+    name: 'cart-item',
+    props: ['product'],
+    data() {
+        return {
+            cartAPI: this.$root.$refs.cart,
+        };
+    },
+    template:  `<div class="cart__item">
+                    <span style="min-width: 10%;">{{ product.id }}</span>
+                    <span><a href="#"><img :src="product.img" alt="some img" style="width: 30px;"></a></span>
+                    <span>{{ product.title }}</span>
+                    <span>{{ product.price }} &#8381;</span>
+                    <div class="cart__item_quantity">
+                        <span @click="cartAPI.decreaseQuantity(product.id)">&minus;</span>
+                        <p>{{ product.quantity }}</p>
+                        <span @click="cartAPI.increaseQuantity(product.id)">&plus;</span>
+                    </div>
+                    <span>{{ product.amount }} &#8381;</span>
+                    <span @click="cartAPI.deleteFromCart(product)" class="cart__item_x" style="min-width: 5%;">&times;</span>
+                </div>`
+};
+
+Vue.component('cart-items', {
+    props: ['cart'],
+    components: {cartItem},
+    template:  `<div class="cart__items">
+                    <div v-if="cart.length == 0" class="cart__item cart__item_plug">
+                        <span>Список товаров пуст. Добавьте товар в корзину.</span>
+                    </div>
+                    <cart-item
+                        v-else
+                        v-for="item of cart"
+                        :key="item.id"
+                        :product="item"
+                    ></cart-item>
+                </div>`
+});
