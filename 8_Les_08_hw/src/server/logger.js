cec2a47e01a157dfe37299b2dd46b33257013ec2
v-shchenyatskiy <9137644477@mail.ru>
2021-02-18 14:38:49 +0700

    ДЗ-8:   - разобрал приложение по модулям   - поправил функцию удаления в скриптах для винды   - сделал сборку через вебпак на дев и на прод

---
+++ 8_Les_08_hw/src/server/logger.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const path = require('path');
+
+/**
+ * Используем path дабы избежать проблем с относительными путями до файлов. Делаем их абсолютными.
+ */
+const logFile = path.resolve(__dirname, './db/2_log.json');
+
+const logger = ( action, product=null ) => {
+    fs.readFile(logFile, 'utf-8', (err, data) => {
+        if (err) {
+            console.log(err);
+        } else {
+            // парсим текущую корзину
+            let log = JSON.parse(data);
+
+            // обновляем лог
+            if (!product) {
+                log.content.push( {N: log._id, action:action, timestamp: new Date().toLocaleString('ru')} );
+            } else {
+                const { id, title, price, quantity, amount } = product;
+            
+                log.content.push({
+                    N: log._id,
+                    action:action,
+                    id:id,
+                    title:title,
+                    price:price,
+                    quantity:quantity,
+                    amount:amount,
+                    timestamp: new Date().toLocaleString('ru')
+                });
+            }
+            log._id++;
+
+            // пишем обратно
+            fs.writeFile( logFile, JSON.stringify(log, null, 4), err => console.log(err) );
+        }
+    });
+};
+
+module.exports = logger;
