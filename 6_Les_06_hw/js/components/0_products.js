1575b8629b1a18bdec477f15ec1f695233dd9203
v-shchenyatskiy <60130407+v-shchenyatskiy@users.noreply.github.com>
2021-03-03 19:40:19 +0700

    6 lesson 06 hw (#6)
    * ДЗ-6:
      - сделал все через компоненты
      - не стал оборачивать функции add/remove/итд в getJSON, т.к. пока не понимаю, зачем все время дергать сервер

---
+++ 6_Les_06_hw/js/components/0_products.js
@@ -0,0 +1,56 @@
+'use strict';
+
+Vue.component('products', {
+    data() {
+        return {
+            filtered: [],
+            products: [],
+            catalogUrl: "/catalogData.json",
+            imgDefault: "https://clck.ru/SurfU",
+        };
+    },
+    methods: {
+        filterProducts(value) {
+            if (!value) { this.filtered = this.products; return; }
+
+            value = value.toLowerCase();
+
+            this.filtered = this.products.filter(item => {
+                if ( item.title.toLowerCase().includes(value) ) { return item; };
+            });
+        },
+    },
+    created() {
+        this.$root.getJSON(`${API + this.catalogUrl}`)
+            .then(products => {
+                for (const {id_product:id, product_name:title, price} of products) {
+                    this.products.push( {id, title, price, img:this.imgDefault} );
+                }
+                this.filtered = this.products;
+            });
+    },
+    template:  `<div class="products">
+                    <product
+                        v-for="product of filtered"
+                        :key="product.id"
+                        :product="product"
+                    ></product>
+                </div>`
+});
+
+Vue.component('product', {
+    props: ['product'],
+    data() {
+        return {
+            quantity: 1,
+            cartAPI: this.$root.$refs.cart,
+        };
+    },
+    template:  `<div class="product__item">
+                    <h3>{{ product.title }}</h3>
+                    <img :src="product.img" alt="some img">
+                    <p>{{ product.price }} &#8381;</p>
+                    <input v-model.number="quantity" type="number" min="1" step="1">
+                    <button @click="cartAPI.addToCart(product, quantity)" class="buy__btn">Добавить в корзину</button>
+                </div>`
+});
